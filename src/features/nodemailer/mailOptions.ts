c88050dd65e387e2b8274e9bde68c3bd256c77fb
8AGaming <ap770ai@gmail.com>
2023-12-18 10:41:19 +0200

    nothing

--- src/features/nodemailer/mailOptions.ts
+++
@@ -1,8 +0,0 @@
-export const mailOptions = () => {
-  return {
-    from: "youremail@gmail.com",
-    to: "myfriend@yahoo.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
-  };
-};
--- src/features/nodemailer/transporter.ts
+++
@@ -1,9 +0,0 @@
-import nodemailer from "nodemailer";
-
-export const transporter = nodemailer.createTransport({
-  service: "fastmail",
-  auth: {
-    user: "asafaiperets@fastmail.com",
-    pass: "Aa123456789@",
-  },
-});
