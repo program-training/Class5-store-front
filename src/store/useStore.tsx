2af4dff62d49fa94605b371406998b48959a3612
ariel gabay <o0o0.wvvw.0o0o@gmail.com>
2023-12-19 10:51:30 +0200

    ;;

--- src/store/useStore.tsx
+++
@@ -1,9 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-
-const useRedux = () => {
-  const dispatch = useDispatch();
-  return {
-    cartItems: useSelector((store) => store.cart.cartItems),
-  };
-};
-export default useRedux;
